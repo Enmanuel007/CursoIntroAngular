7d82da86de944acf0fb6eb5b7176500a647910d6
Enmanuel <eochoa@umariana.edu.co>
2020-07-07 11:31:54 -0500

    Ciclo de vida de un componente en Angular

--- src/app/componentes/producto.componente.ts
+++ src/app/componentes/producto.componente.ts
@@ -1,14 +1,44 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core'; 
+import {Component, 
+    Input, 
+    Output, 
+    EventEmitter, 
+    OnChanges, 
+    SimpleChanges, 
+    OnInit, DoCheck, 
+    OnDestroy
+} from '@angular/core'; 
 import {Producto} from '../producto.model'; 
 
 @Component({
     selector: 'app-producto',
     templateUrl: './producto.componente.html'
 })
-export class ProductoComponente {
+export class ProductoComponente implements OnInit, DoCheck, OnDestroy{
 
     @Input() producto: Producto ;
     @Output() productoClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor(){
+        console.log('1. constructor');
+    }
+
+    //ngOnChanges(changes:SimpleChanges ){
+    //    console.log('2. ngOnChanges');
+    //    console.log(changes);
+    //}
+
+    ngOnInit(){
+        console.log('3. ngOnInit');
+    }
+
+    ngDoCheck() {
+        console.log('4. ngDoCheck');
+        
+    }
+
+    ngOnDestroy(){
+        console.log('5. ngOnDestroy');
+    }
 
     agregarAlCarrito(){
         console.log('añadir al carrito');
